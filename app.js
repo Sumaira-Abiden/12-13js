622478c08a0c9ed683600d9d5b41a72f6ee6a52f
asadraza97 <muhammadasad00400@gmail.com>
2024-11-17 19:44:13 +0500

    12 to 13

---
+++ app.js
@@ -0,0 +1,129 @@
+//Q1
+// var userInput = prompt("Enter Something")
+// // var code = userInput.charCodeAt()
+// var code = Number(userInput)
+
+// // alert(code) 
+// if (code >= 97 && code <= 122 ){
+//     alert('You have type a small letter')
+
+// }
+// if(code >= 65 && code <= 90){
+//     alert('You have type a capital letter')
+// }
+// if ( code >= 48 && code <= 57){
+//     alert('You have type a number')
+
+// }
+
+
+//Q2
+// var integers1=prompt("Enter the first integar")
+// var integers2=prompt("Enter the second integar")
+
+ 
+// if(integers1 > integers2){
+//     alert("integers  1 is greater")
+// }
+// else if(integers2 > integers1){
+//     alert("integers  2 is greater")
+// }
+// else{
+//     alert("Both integers are same")
+// }
+
+//Q3
+// var number =prompt("Enter a number")
+// number=Number(number)
+
+// if(number > 0){
+//     alert("Number is positive")
+// }
+
+// else if(number < 0){
+//     alert("Number is Negative")
+// }
+// else {
+//     alert("Number is zero")
+// }
+
+//Q4
+
+// var name = prompt('Take character')
+// if(name == "a"){
+//     document.write("a is a vowel")
+// }else if(name == "e"){
+//     document.write("e is a vowel")
+// }else if(name == "i"){
+//     document.write("i is a vowel")
+// }else if(name == "o"){
+//     document.write("o is a vowel")
+// }else if (name == "u"){
+//     document.write("u is a vowel")
+// }
+// else {
+//     document.write ("isn't a vowel")
+// }
+
+//Q5
+// var correctPassword= 446258
+// var userPassword = prompt("Enter a password")
+
+// if (userPassword == " ") {
+//     alert("Please enter your password")
+// }
+// else if(userPassword == correctPassword){
+// alert("Correct!  The password you entered matches the original password")
+// }
+// else{
+//     alert("Password isn't match")
+// }
+
+// Q6
+
+// var greeting;
+// var hour = 13
+// if (hour < 18 ){
+//     greeting = 'Good day';
+//     alert(greeting)
+
+// }else{
+//     greeting = 'Good Evening'
+//     alert(greeting)
+// }
+   
+//Q7
+// var clock = prompt("Enter a time")
+// var time = 1900
+
+// if (time >= '0000' && time < 1200){
+//     alert("Good Morning")
+// }
+// else if(time >= 1200 && time < 1700 ){
+//     alert("Good afternoon")
+// }
+// else if (time >=1700 && time < 2100){
+//     alert("Good Evening")
+// }
+// else if (time >= 2100 && time < 2359){
+//     alert("Good night")
+// }
+// else {
+//     alert("Good night :-)")
+// }
+
+
+// var clock = prompt('Enter time')
+// var time = 1900
+// if(time >= '0000' && time < 1200){
+//     alert('Good Morning')
+// }else if (time >= 1200 && time < 1700){
+//     alert('Good Evening')
+// }else if (time >= 1700 && time < 2100){
+//     alert('Good Evening')
+// }else if(time >= 2100 && time < 2359){
+//     alert('Good Night')
+// }else{
+//     alert('Good Night')
+// }
+// console.log(clock)
